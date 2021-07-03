16b6f937a78e9c7bcfd85ab767f1daf154f6a531
antranvn <andevstudioth@gmail.com>
2021-07-03 13:56:36 +0700

    Change to arrow function syntax style

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import './Expenses.css'
 
-function Expenses(props) {
+const Expenses = (props) => {
       return (
         <Card className="expenses">
           <ExpenseItem
