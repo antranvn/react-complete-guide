a2e38b6e288686a208004da5890a2db8d38ffb77
antranvn <andevstudioth@gmail.com>
2021-07-03 19:21:50 +0700

    Listen click event and working with "State"

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,15 @@
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle('Updated!');
+        console.log(title);
+    };
     return (
         <Card className="expense-item">
             <ExpenseDate date ={props.date} />
@@ -10,6 +17,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
+            <button onClick= {clickHandler}>Change Title</button>
         </Card>
     );
 }
