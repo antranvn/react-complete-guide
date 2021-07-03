02180b54f008c70ef5c3623b98013811ad615750
antranvn <andevstudioth@gmail.com>
2021-07-03 19:38:05 +0700

    Fix issue state not applied to title, because we are using props.title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -14,7 +14,7 @@ const ExpenseItem = (props) => {
         <Card className="expense-item">
             <ExpenseDate date ={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
             <button onClick= {clickHandler}>Change Title</button>
