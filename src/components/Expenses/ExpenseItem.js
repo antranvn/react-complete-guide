4e14c9c33a48e709b7a2f8d78241a3f0085ab3ee
antranvn <andevstudioth@gmail.com>
2021-07-04 17:11:53 +0700

    Add expenses filter and lifting the state up

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -15,7 +15,7 @@ const ExpenseItem = (props) => {
             <ExpenseDate date ={props.date} />
             <div className="expense-item__description">
                 <h2>{title}</h2>
-                <div className="expense-item__price">{props.amount}</div>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
             <button 
                 onClick= {clickHandler}>Change Title
